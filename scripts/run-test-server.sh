a7c68c7a1aaeffa4ba9bf8024d5c69affe5c7384
Martin Stadler <mastadle@phys.ethz.ch>
2024-05-22 10:29:07 +0200

    Fix run-test-server.py and print server output at the end

--- scripts/run-test-server.sh
+++ scripts/run-test-server.sh
@@ -5,8 +5,10 @@ if [ ! -f ionpulse_seq_plot.json ]; then
 	poetry -C ./test/ run python ./test/plot_example.py --jsononly &> /dev/null
 fi
 if [ -f ionpulse_seq_plot.json ]; then
-	poetry -C ./test/ run python ./test/sequence_server.py --file ionpulse_seq_plot.json &>> test_server.log &
+	poetry -C ./test/ run python ./test/sequence_server.py \
+		--plot ./test/ionpulse_seq_plot.json \
+		--file ./test/ionpulse_seq.json &>> test_server.log &
 fi
-trap "if ps -p $! > /dev/null ; then kill $!; fi" 0
+trap "if ps -p $! > /dev/null ; then kill $!; cat test_server.log; fi" 0
 vitest
 
