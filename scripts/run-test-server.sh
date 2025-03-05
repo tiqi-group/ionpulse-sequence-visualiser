903784eb0f2de5f566b006871f65bbebdfa4d7ec
Martin Stadler <mastadle@phys.ethz.ch>
2025-03-05 11:56:07 +0100

    Make the JSON test input filename configurable

--- scripts/run-test-server.sh
+++ scripts/run-test-server.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
+json_file="./ionpulse_seq.json"
+if [ -n "$1" ]; then
+	json_file="$1"
+fi
+
 date > test_server.log
-if [ ! -f ./ionpulse_seq.json ]; then
+if [ ! -f $json_file ]; then
 	echo "Generating sequence JSON"
 	poetry -C ./test/ run bash -c 'python $VIRTUAL_ENV/src/ionpulse_sequence_generator/test/ionpulse_seq_gen_test.py' &> test_json_generator.log
 	if [ $? -ne 0 ]; then
 		cat test_json_generator.log
 		exit 1
 	fi
+	json_file="./ionpulse_seq.json"
 	mv ./test/ionpulse_seq.json ./
 fi
-if [ -f ./ionpulse_seq.json ]; then
+if [ -f $json_file ]; then
+	echo "Serving file $json_file"
 	poetry -C ./test/ run python ./sequence_server.py \
-		--file ../ionpulse_seq.json &>> test_server.log &
+		--file ../$json_file &>> test_server.log &
 
 	sleep 0.5
 	if ps -p $! > /dev/null; then
