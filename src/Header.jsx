b86e1d854cf09fd390f29189be6e053c178fd2c0
Martin Stadler <mastadle@phys.ethz.ch>
2025-06-26 13:40:43 +0200

    Display version in banner

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,12 +2,15 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import packageJson from "/package.json";
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="./plot">Sequence Visualizer</Navbar.Brand>
+        <Navbar.Brand href="./plot">
+          Sequence Visualizer {packageJson.version}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
